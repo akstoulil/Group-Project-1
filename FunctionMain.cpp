1143c58f82178d204177f0848648ddb0d99e3539
CrazyCarl78 <craigcrlsn4@gmail.com>
2018-02-26 11:14:54 -0600

    Add files via upload

---
+++ FunctionMain.cpp
@@ -0,0 +1,53 @@
+//#include "stdafx.h"
+#include "CardClass.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+inline void SetupDeck(CardClass arr[][13]) {
+
+	static string numberToSuit[4] = { "Hearts", "Diamonds", "Clubs", "Spades" };
+	static string numberToName[13] = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+
+	for (int counterOne = 0; counterOne < 4; counterOne++) {
+		for (int counterTwo = 0; counterTwo < 13; counterTwo++) {
+			//I've forgotten the proper syntax for this
+
+			if (counterTwo > 9 || counterTwo == 9) {
+
+				int faceValue = 10;
+
+				CardClass c = CardClass::CardClass();
+				c.setSuitName(numberToSuit[counterOne]);
+				c.setNumberName(numberToName[counterTwo]);
+				c.setSuit(counterOne);
+				c.setNumber(faceValue);
+				c.setState(false);
+
+				arr[counterOne][counterTwo] = c;
+
+			}
+			else {
+
+				CardClass c = CardClass::CardClass();
+				c.setSuitName(numberToSuit[counterOne]);
+				c.setNumberName(numberToName[counterTwo]);
+				c.setSuit(counterOne);
+				c.setNumber(counterTwo + 1);
+				c.setState(false);
+
+				arr[counterOne][counterTwo] = c;
+
+			}
+
+			/*
+			arr[counterOne][counterTwo].setSuit(counterOne);
+			arr[counterOne][counterTwo].setNumber(counterTwo);
+			arr[counterOne][counterTwo].setState(false);
+			*/
+
+		}
+	}
+
+	//return arr; //I don't know why this is erroring
+}
---
+++ PlayerClass.h
@@ -0,0 +1,201 @@
+#pragma once
+#include "CardClass.h"
+#include <vector>
+#include <random>
+#include <iostream>
+#include <time.h>
+
+class PlayerClass
+{
+protected:
+	int chipsToBet;
+	int card_total;
+	bool playerChoice;
+	int chipsToPlay;
+	vector<CardClass> currentHand;
+
+public:
+
+	PlayerClass() {
+
+		chipsToBet = 0;
+		card_total = 0;
+		chipsToPlay = 0;
+
+	}
+
+	PlayerClass(int chipsToBet, int card_total, bool playerChoice, int chipsToPlay)
+	{
+		chipsToBet = 0;
+		card_total = 0;
+		playerChoice = false;
+		chipsToPlay = 0;
+	}
+
+	int setchipsToBet(int b)
+	{
+		chipsToBet = b;
+	}
+	int setCard_total(int t)
+	{
+		card_total = t;
+	}
+	bool setPlayerChoice(bool p)
+	{
+		playerChoice = p;
+	}
+	int setchipsToPlay(int ctp)
+	{
+		chipsToBet = ctp;
+	}
+
+	int getchipsToBet() const
+	{
+		return chipsToBet;
+	}
+
+	int getCard_total() const
+	{
+		return card_total;
+	}
+
+	bool getPlayerChoice() const
+	{
+		return playerChoice;
+	}
+	int getchipsToPlay() const
+	{
+		return chipsToPlay;
+	}
+
+	void initializeHand(CardClass arr[][13]) {
+
+		int counter = 0;
+
+		while (counter < 2) {
+
+			srand(time(NULL));
+
+			int randomSuit = rand() % 4;
+			int randomNumber = rand() % 13;
+
+			if (arr[randomSuit][randomNumber].getState() == false) {
+
+				CardClass card = arr[randomSuit][randomNumber];
+				currentHand.push_back(card);
+
+				card_total += card.getNumber();
+
+				card.displayCardInfo();
+
+
+				arr[randomSuit][randomNumber].setState(true);
+
+				counter++;
+
+			}
+		}
+
+		cout << "Your current total is: " << getCard_total() << endl;
+
+	}
+	void dealerInitializeHand(CardClass arr[][13]) {
+
+		int counter = 0;
+
+		while (counter < 2) {
+
+			srand(time(NULL));
+
+			int randomSuit = rand() % 4;
+			int randomNumber = rand() % 13;
+
+			if (arr[randomSuit][randomNumber].getState() == false) {
+
+				CardClass card = arr[randomSuit][randomNumber];
+				currentHand.push_back(card);
+
+				card_total += card.getNumber();
+
+				card.dealerCardInfo();
+
+				arr[randomSuit][randomNumber].setState(true);
+
+				counter++;
+
+			}
+		}
+
+		cout << "The dealer's current total is: " << getCard_total() << endl;
+
+	}
+
+	void addCard(CardClass arr[][13]) {
+
+		bool cardDrawn = false;
+
+		while (cardDrawn == false) {
+
+			srand(time(NULL));
+
+			int randomSuit = rand() % 4;
+			int randomNumber = rand() % 13;
+
+			if (arr[randomSuit][randomNumber].getState() == false) {
+
+				CardClass card = arr[randomSuit][randomNumber];
+				currentHand.push_back(card);
+
+				card_total += card.getNumber();
+				arr[randomSuit][randomNumber].setState(true);
+
+				card.displayCardInfo();
+
+				cardDrawn = true;
+
+			}
+		}
+
+		cout << "Your current total is: " << getCard_total() << endl;
+
+	}
+
+	void addDealerCard(CardClass arr[][13]) {
+
+		bool cardDrawn = false;
+
+		while (cardDrawn == false) {
+
+			srand(time(NULL));
+
+			int randomSuit = rand() % 4;
+			int randomNumber = rand() % 13;
+
+			if (arr[randomSuit][randomNumber].getState() == false) {
+
+				CardClass card = arr[randomSuit][randomNumber];
+				currentHand.push_back(card);
+
+				card_total += card.getNumber();
+				arr[randomSuit][randomNumber].setState(true);
+
+				card.dealerCardInfo();
+
+				cardDrawn = true;
+
+			}
+		}
+
+		cout << "The dealer's current total is: " << getCard_total() << endl;
+
+	}
+
+	void clearHand() {
+
+		currentHand.clear();
+		card_total = 0;
+
+	}
+};
+
+
